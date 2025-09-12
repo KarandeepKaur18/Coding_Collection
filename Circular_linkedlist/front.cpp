c6b8fdc6369f7c484ead8fcc8543d6392a6a8fe7
karandeepkaur18 <karandeepkaur7718@gmail.com>
2025-09-12 12:14:22 +0530

    Added Circular LINKEDLIST

---
+++ Circular_linkedlist/front.cpp
@@ -0,0 +1,63 @@
+#include <iostream>
+using namespace std;
+
+class Node{
+    public:
+    int data;
+    Node* next;
+
+    Node(int val){
+        data = val;
+        next = NULL;
+    }
+};
+
+class CircularList{
+    Node* head;
+    Node* tail;
+
+    CircularList(){
+        head = tail =NULL;
+    }
+
+    void add_front(int val){
+        Node* newnode = new Node(val);
+        if(head == NULL){
+            head = tail = newnode;
+            tail->next = head;
+            return;
+        }
+
+        newnode->next = head;
+        head = newnode;
+        tail->next = head;
+
+    }
+
+    void print(){
+        if(head == NULL){
+            return;
+        }
+
+        cout << head->data << "->";
+        Node* temp = head->next;
+
+        while(temp != head){
+            cout << temp->data << "->";
+            temp = temp->next;
+        }
+
+        cout << temp->data <<endl;
+        // to show that it is circular ll
+    }
+};
+
+
+int main(){
+    CircularList cll;
+    
+    cll.add_front(1);
+    cll.add_front(2);
+    cll.add_front(3);
+
+}
